e01cf376acbda7889f88a9f61921327eba09b169
kostas <akwstas1@gmail.com>
2022-11-03 23:08:19 +0200

    Change like/dislike symbols.

--- index.js
+++ index.js
@@ -1,70 +1,20 @@
 const containerArr = Array.from(document.querySelectorAll('div.cards-container'));
-// console.log(containerArr.length)
-// console.log(containerArr[0].getAttribute('status'))
 
-
-// // window.onload = () => {
-//     for (const container of containerArr) {
-//         if (container.getAttribute('status') === 'hidden') {
-//             container.style.transform = 'scale(0)';
-//         }
-//     }
-// // }
-
-// const like = param => {
-//     for (let i in containerArr) {
-//         let status = containerArr[i].getAttribute('status');
-//         let index;
-//         if (status === 'visible') {
-//             if (i == containerArr.length - 1) {
-//                 index = 0;
-//             } else {
-//                 i = Number(i)
-//                 index = i + 1;
-//             }
-//             console.log(containerArr)
-//             if (param === 'favorite') {
-//                 console.log('shrinking animation')
-//                 containerArr[index].animate(shrinkingRight, shrinkTiming)
-//                 console.log('Expanding animation')
-//                 containerArr[i].animate(expandingRight, expandTiming)
-//             } else {
-//                 containerArr[index].animate(shrinkingLeft, shrinkTiming)
-//                 containerArr[i].animate(expandingLeft, expandTiming)
-//             }
-//             console.log('is visible')
-//             containerArr[index].setAttribute('status', 'visible');
-//             containerArr[index].style.display = 'block';
-//             containerArr[i].setAttribute('status', 'hidden');
-//             containerArr[i].style.display = 'none';
-//             break;
-//         }
-//     }
-// }
-
-
-// document.querySelector('like').animate()
 const swipe = param => {
     const visibleContainer = containerArr.find(item => item.getAttribute('status') === 'visible');
-    // console.log(`visibleContainer ${visibleContainer.getAttribute('status')}`);
     const indexOfVisibleContainer = containerArr.indexOf(visibleContainer);
-    // console.log(`indexOfVisibleContainer ${indexOfVisibleContainer}`);
     let nextIndexOfVisibleContainer, nextVisibleContainer;
     switch (param) {
         case 'right':
             nextIndexOfVisibleContainer = indexOfVisibleContainer === containerArr.length - 1 ? 0 : indexOfVisibleContainer + 1;
-            // console.log(`nextIndexOfVisibleContainer ${nextIndexOfVisibleContainer}`);
             nextVisibleContainer = containerArr[nextIndexOfVisibleContainer];
             visibleContainer.setAttribute('status', 'hidden');
             nextVisibleContainer.setAttribute('status', 'visible');
             visibleContainer.animate(shrinkingRight, shrinkTiming);
-            // console.log(visibleContainer.getAttribute('status'))
-            // console.log(nextVisibleContainer.getAttribute('status'))
             nextVisibleContainer.animate(expandingRight, expandTiming);
             break;
         case 'left':
             nextIndexOfVisibleContainer = indexOfVisibleContainer === 0 ? containerArr.length - 1 : indexOfVisibleContainer - 1;
-            // console.log(`nextIndexOfVisibleContainer ${nextIndexOfVisibleContainer}`);
             nextVisibleContainer = containerArr[nextIndexOfVisibleContainer];
             visibleContainer.animate(shrinkingLeft, shrinkTiming);
             nextVisibleContainer.animate(expandingLeft, expandTiming);
